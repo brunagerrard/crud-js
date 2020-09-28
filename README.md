caedd85a2f662534b476ad2fa8dee9006343aad5
Bruna Nolasco <brunamnolasco@gmail.com>
2020-09-28 11:54:55 -0300

    estilos e readme

---
+++ README.md
@@ -0,0 +1,3 @@
+### CRUD com JavaScript
+
+Aplicação de baixa complexidade que realiza as 4 operações de CRUD *(create, read, update, delete)* em JavaScript puro. A app foi construída tendo como base uma aula do prof. Raphael Gomide, do IGTI.
--- script.js
+++ script.js
@@ -1,101 +1,102 @@
-window.addEventListener('load', start)
+window.addEventListener("load", start);
 
-var globalNames = [1, 2, 3, 4]
-var inputName = null
-var currentIndex = null
-var isEditing = false
+var globalNames = ['Bruna', 'Steven', 'Fidel'];
+var inputName = null;
+var currentIndex = null;
+var isEditing = false;
 
 function start() {
-  inputName = document.querySelector('#nomeInput')
-  preventFormSubmit()
-  activateInput()
-  render()
+  inputName = document.querySelector("#nomeInput");
+  preventFormSubmit();
+  activateInput();
+  render();
 }
 
 function preventFormSubmit() {
-  var form = document.querySelector('form')
+  var form = document.querySelector("form");
 
-  form.addEventListener('submit', handleFormSubmit)
+  form.addEventListener("submit", handleFormSubmit);
 
   function handleFormSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
   }
 }
 
 function activateInput() {
   function insertName(newName) {
-    globalNames.push(newName)
+    globalNames.push(newName);
   }
 
   function updateName(newName) {
-    globalNames[currentIndex] = newName
+    globalNames[currentIndex] = newName;
   }
 
   function handleTyping(e) {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       if (isEditing) {
-        updateName(e.target.value)
-      } else { 
-        insertName(e.target.value) 
+        updateName(e.target.value);
+      } else {
+        insertName(e.target.value);
       }
-      isEditing = false
-      clearInput()
-      render()
+      isEditing = false;
+      clearInput();
+      render();
     }
   }
 
-  inputName.addEventListener('keyup', handleTyping)
-  inputName.focus()
+  inputName.addEventListener("keyup", handleTyping);
+  inputName.focus();
 }
 
 function render() {
   function createDelButton(index) {
     function deleteName() {
-      globalNames.splice(index, 1)
-      render()
-    }    
-    var delButton = document.createElement('button')
-    delButton.textContent = 'x'
-    delButton.addEventListener('click', deleteName)
-    return delButton
+      globalNames.splice(index, 1);
+      render();
+    }
+    var delButton = document.createElement("button");
+    delButton.textContent = "x";
+    delButton.className = "mybutton";
+    delButton.addEventListener("click", deleteName);
+    return delButton;
   }
 
   function createSpan(name, index) {
     function editItem() {
-      inputName.value = name
-      inputName.focus()
-      isEditing = true
-      currentIndex = index
+      inputName.value = name;
+      inputName.focus();
+      isEditing = true;
+      currentIndex = index;
     }
-    var span = document.createElement('span')
-    span.textContent = name
-    span.addEventListener('click', editItem)
-    return span
+    var span = document.createElement("span");
+    span.textContent = name;
+    span.addEventListener("click", editItem);
+    return span;
   }
 
-  var divNames = document.querySelector('#names')
-  divNames.innerHTML = ''
+  var divNames = document.querySelector("#names");
+  divNames.innerHTML = "";
 
-  var ul = document.createElement('ul')
-  
+  var ul = document.createElement("ul");
+
   for (var i = 0; i < globalNames.length; i++) {
-    var currentName = globalNames[i]
+    var currentName = globalNames[i];
 
-    var li = document.createElement('li')
-    var delButton = createDelButton(i)
-    var span = createSpan(currentName, i)
+    var li = document.createElement("li");
+    var delButton = createDelButton(i);
+    var span = createSpan(currentName, i);
 
-    li.appendChild(delButton)
-    li.appendChild(span)
-    ul.appendChild(li)
+    li.appendChild(delButton);
+    li.appendChild(span);
+    ul.appendChild(li);
   }
 
-  divNames.appendChild(ul)
+  divNames.appendChild(ul);
 
-  clearInput()
+  clearInput();
 }
 
 function clearInput() {
-  inputName.value = ''
-  inputName.focus()
+  inputName.value = "";
+  inputName.focus();
 }
